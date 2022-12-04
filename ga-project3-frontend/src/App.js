82cdeef3fa4f9c756c90b7fee17a8b0e09e98d50
Midzi Ribery <muhd.termidzi@gmail.com>
2022-12-04 20:18:37 +0800

    playing with bootstrap

--- ga-project3-frontend/src/App.js
+++ ga-project3-frontend/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Clean Slate</h1>
+      <Button>This is a button</Button>
     </div>
   );
 }
