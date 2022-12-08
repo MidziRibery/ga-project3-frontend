6e08c4dde7edfdfd285f93f1c7e8a7b88c14cd30
Midzi Ribery <muhd.termidzi@gmail.com>
2022-12-08 21:07:00 +0800

    Logo added

--- ga-project3-frontend/src/components/Menu.js
+++ ga-project3-frontend/src/components/Menu.js
@@ -1,13 +1,40 @@
 import React from 'react';
 import styled from 'styled-components';
+import ComfortTube from '../img/YT Logo.jpg';
 
 const Container = styled.div`
-    flex:1
+    flex:1;
+    background-color: #202020;
+    height: 100vh;
+    color: white;
+    font-size: 14px;
+`
+
+const Wrapper = styled.div`
+    padding: 18px 26px;
+`
+
+const Logo = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 5px;
+    font-weight:bold;
+    margin-bottom: 25px;
+`
+const Img = styled.img`
+    height: 25px;
 `
 
 export const Menu = () => {
   return (
-    <Container>Menu</Container>
+    <Container>
+        <Wrapper>
+            <Logo>
+                <Img src={ComfortTube}/>
+                ComfortTube
+            </Logo>
+        </Wrapper>
+    </Container>
   )
 }
 
